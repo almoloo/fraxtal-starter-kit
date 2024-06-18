a89235a1d5264a1c671da6acda9ab7a48cabeb48
Ali Mousavi <amousavig@icloud.com>
2024-06-18 21:29:27 +0330

    Finished fetch json.

--- app/api/v1/ipfs/fetch/route.ts
+++ app/api/v1/ipfs/fetch/route.ts
@@ -1,26 +1,45 @@
 import { getPinataSettings } from "@/utils/network-configs";
+import { NextResponse } from "next/server";
+// import PinataClient from "@pinata/sdk";
+
+// const pinata = new PinataClient({
+//   pinataApiKey: process.env.PINATA_API_KEY,
+//   pinataSecretApiKey: process.env.PINATA_API_SECRET,
+// });
 
 export const dynamic = "force-dynamic";
 
-export async function GET(request: Request): Promise<Response> {
+export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const hash = searchParams.get("hash");
   const gatewayUrl = getPinataSettings().gateway;
 
   if (!gatewayUrl) {
-    return new Response("Pinata gateway URL not found.", { status: 500 });
+    return NextResponse.json(
+      { message: "Pinata gateway URL not found." },
+      { status: 500 }
+    );
   }
   if (!hash) {
-    return new Response("IPFS hash not found.", { status: 422 });
+    return NextResponse.json(
+      { message: "IPFS hash not found." },
+      { status: 422 }
+    );
   }
 
-  const res = await fetch(`${gatewayUrl}/ipfs/${hash}`);
+  const res = await fetch(
+    `${gatewayUrl}/ipfs/${hash}?pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_GATEWAY_TOKEN}`
+  );
 
   if (!res.ok) {
-    return new Response("Failed to fetch IPFS file." + (await res.text()), {
-      status: 500,
-    });
+    return NextResponse.json(
+      { message: "Failed to fetch IPFS file." } + (await res.text()),
+      {
+        status: 500,
+      }
+    );
   }
+  const jsonRes = await res.json();
 
-  return Response.json({ message: res.body });
+  return NextResponse.json(jsonRes);
 }
