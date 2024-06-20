4746720e7f3b41d423efedd1815b973e5aa8aa2a
Ali Mousavi <amousavig@icloud.com>
2024-06-21 02:39:30 +0330

    Finished internal transaction api.

---
+++ app/api/v1/explorer/account/txlistinternaltx/route.ts
@@ -0,0 +1,31 @@
+import { getExplorerSettings } from "@/utils/network-configs";
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const searchParams = request.nextUrl.searchParams;
+  const txHash = searchParams.get("txhash");
+  const { apiKey, baseUrl } = getExplorerSettings();
+
+  const returnError = (message: string, status: number) => {
+    return NextResponse.json({ message }, { status });
+  };
+
+  if (!apiKey) returnError("API key not found.", 500);
+  if (!baseUrl) returnError("Base URL not found.", 500);
+  if (!txHash) returnError("Address not found.", 422);
+
+  const res = await fetch(
+    `${baseUrl}?module=account&action=txlistinternal&txhash=${txHash}&tag=latest&apikey=${apiKey}`
+  );
+
+  if (!res.ok) returnError("Failed to fetch internal transactions list.", 500);
+
+  const jsonRes = await res.json();
+
+  if (jsonRes.status === "0")
+    returnError("Failed to fetch internal transactions list.", 500);
+
+  return NextResponse.json(jsonRes.result);
+}
