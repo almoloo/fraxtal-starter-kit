7081c7cd5c1f9abeedd1d597b443239924871c02
Ali Mousavi <amousavig@icloud.com>
2024-06-21 03:03:13 +0330

    Finished contract execution check API.

---
+++ app/api/v1/explorer/transaction/getstatus/route.ts
@@ -0,0 +1,31 @@
+import { getExplorerSettings } from "@/utils/network-configs";
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const searchParams = request.nextUrl.searchParams;
+  const txHash = searchParams.get("txhash");
+  const { apiKey, baseUrl } = getExplorerSettings();
+
+  const returnError = (message: string, status: number) => {
+    return NextResponse.json({ message }, { status });
+  };
+
+  if (!apiKey) returnError("API key not found.", 500);
+  if (!baseUrl) returnError("Base URL not found.", 500);
+  if (!txHash) returnError("Address not found.", 422);
+
+  const res = await fetch(
+    `${baseUrl}?module=transaction&action=getstatus&txhash=${txHash}&tag=latest&apikey=${apiKey}`
+  );
+
+  if (!res.ok) returnError("Failed to fetch token transfers list.", 500);
+
+  const jsonRes = await res.json();
+
+  if (jsonRes.status === "0" || jsonRes.isError !== undefined)
+    returnError("Failed to fetch token transfers list.", 500);
+
+  return NextResponse.json(jsonRes.result);
+}
