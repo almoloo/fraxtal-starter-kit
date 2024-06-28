8e3d772edd478f550385ae2a751bf7d5cc7ca522
Ali Mousavi <amousavig@icloud.com>
2024-06-28 21:37:26 +0330

    Finished ethBalance view.

--- app/explorer/components/ethBalance.tsx
+++ app/explorer/components/ethBalance.tsx
@@ -1,38 +1,78 @@
-import { getFrxEthBalance } from "@/utils/data";
-import { EthBalanceResponse } from "@/utils/definitions";
-import { useState } from "react";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { getFrxEthBalance } from '@/utils/data';
+import { EthBalanceResponse } from '@/utils/definitions';
+import { LoaderIcon } from 'lucide-react';
+import { useState } from 'react';
 
 const EthBalance = () => {
-  const [address, setAddress] = useState(
-    "0x69B2AC8192a0c47B9C11543f20c367fcECCed7a3"
-  );
-  const [response, setResponse] = useState<EthBalanceResponse | null>(null);
+	const [address, setAddress] = useState(
+		'0x69B2AC8192a0c47B9C11543f20c367fcECCed7a3'
+	);
+	const [loading, setLoading] = useState<boolean>(false);
+	const [response, setResponse] = useState<EthBalanceResponse | null>(null);
+	const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setLoading(true);
+		setError(null);
+		setResponse(null);
 
-    try {
-      const balance = await getFrxEthBalance([address]);
-      setResponse(balance);
-    } catch (error) {
-      console.error("Failed to fetch frxETH balance:", error);
-    }
-  };
-  return (
-    <div className="m-5 border p-5">
-      <h2>Get frxETH Balance for a single Address</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="address/addresses"
-          defaultValue={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <pre>{JSON.stringify(response, null, 2)}</pre>
-    </div>
-  );
+		try {
+			const balance = await getFrxEthBalance([address]);
+			setResponse(balance);
+		} catch (error: unknown) {
+			console.error('Failed to fetch frxETH balance:', error);
+			setError(error as string);
+		} finally {
+			setLoading(false);
+		}
+	};
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="border rounded p-5 flex flex-col gap-5 mb-5"
+		>
+			<h2 className="font-medium">
+				Get frxETH Balance for a single Address
+			</h2>
+			<section className="flex gap-3">
+				<Input
+					type="text"
+					placeholder="address/addresses"
+					defaultValue={address}
+					onChange={(e) => setAddress(e.target.value)}
+					disabled={loading}
+				/>
+				<Button
+					type="submit"
+					disabled={loading || address.length === 0}
+				>
+					{loading && (
+						<LoaderIcon className="animate-spin w-4 h-4 mr-2" />
+					)}
+					Submit
+				</Button>
+			</section>
+			{response && (
+				<section className="alert green">
+					<h3 className="font-bold mb-1">Fetched Data:</h3>
+					<pre className="text-sm whitespace-break-spaces break-words">
+						{JSON.stringify(response, null, 2)}
+					</pre>
+				</section>
+			)}
+			{error && (
+				<section className="alert red">
+					<p>Failed to fetch frxETH balance:</p>
+					<pre className="mt-3 bg-rose-100/50 p-5 whitespace-break-spaces break-words">
+						{error.toString()}
+					</pre>
+				</section>
+			)}
+		</form>
+	);
 };
 
 export default EthBalance;
--- app/explorer/page.tsx
+++ app/explorer/page.tsx
@@ -1,31 +1,42 @@
-"use client";
-import { getFrxEthBalance } from "@/utils/data";
-import { useState } from "react";
-import EthBalance from "./components/ethBalance";
-import TxList from "./components/txList";
-import TxListInternal from "./components/txListInternal";
-import TxListInternalTx from "./components/txListInternalTx";
-import TokenTx from "./components/tokenTx";
-import TokenNftTx from "./components/tokenNftTx";
-import GetStatus from "./components/getStatus";
-import GetTxReceiptStatus from "./components/getTxReceiptStatus";
-import TokenBalance from "./components/tokenBalance";
+'use client';
+import { getFrxEthBalance } from '@/utils/data';
+import { useState } from 'react';
+import EthBalance from './components/ethBalance';
+import TxList from './components/txList';
+import TxListInternal from './components/txListInternal';
+import TxListInternalTx from './components/txListInternalTx';
+import TokenTx from './components/tokenTx';
+import TokenNftTx from './components/tokenNftTx';
+import GetStatus from './components/getStatus';
+import GetTxReceiptStatus from './components/getTxReceiptStatus';
+import TokenBalance from './components/tokenBalance';
+import { GlobeIcon } from 'lucide-react';
 
 const page = () => {
-  return (
-    <div>
-      <h1>EXPLORER API</h1>
-      <EthBalance />
-      <TxList />
-      <TxListInternal />
-      <TxListInternalTx />
-      <TokenTx />
-      <TokenNftTx />
-      <GetStatus />
-      <GetTxReceiptStatus />
-      <TokenBalance />
-    </div>
-  );
+	return (
+		<div>
+			<header className="mb-5 flex flex-col gap-3">
+				<h1 className="flex items-center font-bold text-xl">
+					<GlobeIcon className="text-neutral-500 mr-2 h-6 w-6" />
+					<span>Fetch Explorer Data</span>
+				</h1>
+				<p className="alert gray leading-relaxed">
+					This interface allows you to fetch data from the Explorer
+					API, which provides information about Fraxtal transactions,
+					token balances, and more.
+				</p>
+			</header>
+			<EthBalance />
+			<TxList />
+			<TxListInternal />
+			<TxListInternalTx />
+			<TokenTx />
+			<TokenNftTx />
+			<GetStatus />
+			<GetTxReceiptStatus />
+			<TokenBalance />
+		</div>
+	);
 };
 
 export default page;
